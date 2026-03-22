e37400078f0a122b90c9596fba5833023d25cd78
unknown <shubh386a@gmail.com>
2026-03-23 02:15:48 +0530

    cors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // this are use for the call backend .Only need to "axiosInstance "
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.PROD ? "https://chat-application-backend-rust.vercel.app/api/v1" : "http://localhost:4800/api/v1");
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.PROD ? "https://chat-application-backend-peach.vercel.app/api/v1" : "http://localhost:4800/api/v1");
 
 export const axiosInstance =axios.create({
     baseURL: API_BASE_URL,
